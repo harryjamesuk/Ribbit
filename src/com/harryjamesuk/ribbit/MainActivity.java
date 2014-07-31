d4c0ef0137e96385e1629714cf8a8340ffb20a10
harryjamesuk <harryjamesuk+github@gmail.com>
2014-07-31 16:44:21 +0100

    Added login screen
    Added some extra strings,
    Created a temporary intent to start our login activity.
    
    Login button currently does nothing.

--- src/com/harryjamesuk/ribbit/MainActivity.java
+++ src/com/harryjamesuk/ribbit/MainActivity.java
@@ -2,21 +2,20 @@ package com.harryjamesuk.ribbit;
 
 import java.util.Locale;
 
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v13.app.FragmentPagerAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class MainActivity extends Activity implements ActionBar.TabListener {
 
@@ -38,6 +37,9 @@ public class MainActivity extends Activity implements ActionBar.TabListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Intent intent = new Intent(this, LoginActivity.class);
+		startActivity(intent);
 
 		// Set up the action bar.
 		final ActionBar actionBar = getActionBar();
