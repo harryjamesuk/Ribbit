0add50b24f365c0d9a9d818486d6031b45f0bfc9
harryjamesuk <harryjamesuk+github@gmail.com>
2014-08-20 11:23:52 +0100

    Users can now be related
    It is now possible to add a friendsRelation between users.
    Currently, you can only add a relation. :heart_eyes:

--- src/com/harryjamesuk/ribbit/ParseConstants.java
+++ src/com/harryjamesuk/ribbit/ParseConstants.java
@@ -5,4 +5,5 @@ public final class ParseConstants {
 	
 	// Field names
 	public static final String KEY_USERNAME = "username";
+	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
 }
