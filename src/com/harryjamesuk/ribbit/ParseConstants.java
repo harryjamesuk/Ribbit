0bf7b15070596c94c06a7722be690577bf3b28f6
harryjamesuk <harryjamesuk+github@gmail.com>
2014-09-05 22:53:46 +0100

    Added preparations for sending message
    Will need a send() method.
    Added createMessage() method that will define our message in a new class
    called "Messages" (e.g: Before we had a class called User where we'd
    store all of our users information).
    In this new class, we will put keys in and tie them to their value.
    
    Also added new getRecipientIds() method that will go through the whole
    list adding those who are checked.
    
    Currently, the message will not be sent as, the send() method has not
    been created and is commented out in this commit to avoid errors.

--- src/com/harryjamesuk/ribbit/ParseConstants.java
+++ src/com/harryjamesuk/ribbit/ParseConstants.java
@@ -2,8 +2,14 @@ package com.harryjamesuk.ribbit;
 
 public final class ParseConstants {
 	// Class names
+	public static final String CLASS_MESSAGES = "Messages";
 	
 	// Field names
 	public static final String KEY_USERNAME = "username";
 	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+	public static final String KEY_RECIPIENT_IDS = "recipientIds";
+	public static final String KEY_SENDER_ID = "senderId";
+	public static final String KEY_SENDER_NAME = "senderName";
+	public static final String KEY_FILE = "file";
+	public static final String KEY_FILE_TYPE = "fileType";
 }
--- src/com/harryjamesuk/ribbit/RecipientsActivity.java
+++ src/com/harryjamesuk/ribbit/RecipientsActivity.java
@@ -1,12 +1,7 @@
 package com.harryjamesuk.ribbit;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
-import com.parse.ParseRelation;
-import com.parse.ParseUser;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
@@ -18,6 +13,13 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
 
 public class RecipientsActivity extends ListActivity {
 	
@@ -96,7 +98,8 @@ public class RecipientsActivity extends ListActivity {
 		int id = item.getItemId();
 		switch (id) {
 		case R.id.action_send:
-			// TODO
+			ParseObject message = createMessage();
+//			send(message);
 		}
 		return super.onOptionsItemSelected(item);
 	}
@@ -111,5 +114,24 @@ public class RecipientsActivity extends ListActivity {
 		else {
 			mSendMenuItem.setVisible(false);
 		}
+	}
+	
+	protected ParseObject createMessage() {
+		ParseObject message = new ParseObject(ParseConstants.CLASS_MESSAGES);
+		message.put(ParseConstants.KEY_SENDER_ID, ParseUser.getCurrentUser().getObjectId());
+		message.put(ParseConstants.KEY_SENDER_NAME, ParseUser.getCurrentUser().getUsername());
+		message.put(ParseConstants.KEY_RECIPIENT_IDS, getRecipientIds());
+		
+		return message;
+	}
+	
+	protected ArrayList<String> getRecipientIds() {
+		ArrayList<String> recipientIds = new ArrayList<String>();
+		for (int i = 0; i < getListView().getCount(); i++) {
+			if (getListView().isItemChecked(i)) {
+				recipientIds.add(mFriends.get(i).getObjectId());
+			}
+		}
+		return recipientIds;
 	}
 }
