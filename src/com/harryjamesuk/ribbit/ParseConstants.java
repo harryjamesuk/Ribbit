1920f1bce4b3da9a94da9cf77f7ee08d5a7830e5
harryjamesuk <harryjamesuk+github@gmail.com>
2014-09-12 18:03:02 +0100

    Messages will now display in InboxFragment
    Added onResume() method. Will query Parse to get our messages class.
    Then, will get all data where the recipientId is equal to that of the
    current user. Then, these will be displayed in descending order upon the
    creation date.

--- src/com/harryjamesuk/ribbit/ParseConstants.java
+++ src/com/harryjamesuk/ribbit/ParseConstants.java
@@ -12,7 +12,8 @@ public final class ParseConstants {
 	public static final String KEY_SENDER_NAME = "senderName";
 	public static final String KEY_FILE = "file";
 	public static final String KEY_FILE_TYPE = "fileType";
-
+	public static final String KEY_CREATED_AT = "createdAt";
+	
 	public static final String TYPE_IMAGE = "image";
 	public static final String TYPE_VIDEO = "video";
 
