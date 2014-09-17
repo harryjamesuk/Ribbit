d0b9fd566b54cee2e5dd8cbddec350e90754aa84
harryjamesuk <harryjamesuk+github@gmail.com>
2014-09-17 17:25:47 +0100

    Preparations for displaying our Image Message
    Created ViewImageActivity.java activity and the layout
    activity_view_image.xml to go with it.
    Updated view portrait only.
    
    InboxFragment updated with an onListItemClick() method. It will get the
    message at the position, the file type and the url to the file on Parse.
    
    If it is an image, we'll start up our ViewImageActivity and pass in the
    Uri (As, at this point, we have converted the Url to a Uri) and then
    start the Intent.

--- src/com/harryjamesuk/ribbit/InboxFragment.java
+++ src/com/harryjamesuk/ribbit/InboxFragment.java
@@ -2,14 +2,18 @@ package com.harryjamesuk.ribbit;
 
 import java.util.List;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -59,4 +63,24 @@ public class InboxFragment extends ListFragment {
 		});
 	}
 
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		ParseObject message = mMessages.get(position);
+		String messageType = message.getString(ParseConstants.KEY_FILE_TYPE);
+		ParseFile file = message.getParseFile(ParseConstants.KEY_FILE);
+		Uri fileUri = Uri.parse(file.getUrl());
+		
+		if (messageType.equals(ParseConstants.TYPE_IMAGE)) {
+			// View the image
+			Intent intent = new Intent(getActivity(), ViewImageActivity.class);
+			intent.setData(fileUri);
+			startActivity(intent);
+		}
+		else {
+			// View the video
+		}
+	}
+	
 }
---
+++ src/com/harryjamesuk/ribbit/ViewImageActivity.java
@@ -0,0 +1,40 @@
+package com.harryjamesuk.ribbit;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+public class ViewImageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_image);
+		
+		ImageView imageView = (ImageView)findViewById(R.id.imageView);
+		
+		Uri imageUri = getIntent().getData();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.view_image, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
