fbab63f32592f4977d2a0f74d4ada95e44930f4d
harryjamesuk <harryjamesuk+github@gmail.com>
2014-09-18 19:35:08 +0100

    Images will now time out
    Added a Timer that will finish the activity after 10 seconds (1000ms).

--- src/com/harryjamesuk/ribbit/ViewImageActivity.java
+++ src/com/harryjamesuk/ribbit/ViewImageActivity.java
@@ -1,10 +1,11 @@
 package com.harryjamesuk.ribbit;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -21,5 +22,14 @@ public class ViewImageActivity extends Activity {
 		Uri imageUri = getIntent().getData();
 		
 		Picasso.with(this).load(imageUri.toString()).into(imageView);
+	
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				finish();				
+			}
+		}, 10*1000);
 	}
 }
