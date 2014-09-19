91a69f1a6c60fe28c419a132bd52eb508287c778
Harry James <harryjamesuk@users.noreply.github.com>
2014-09-19 20:05:12 +0100

    Update RibbitApplication.java

--- src/com/harryjamesuk/ribbit/RibbitApplication.java
+++ src/com/harryjamesuk/ribbit/RibbitApplication.java
@@ -9,7 +9,7 @@ public class RibbitApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		  Parse.initialize(this, "2ndNNbwQ0zwOjPj615blaeXGCQi6EIXoRPGknSpH", "oofvpXzWyVqrt6wRBigvDzv1aGNl644Gdwkw6leC");
+**		  Parse.initialize(this, "HIDDEN", "HIDDEN");
 	
 	}
 }
