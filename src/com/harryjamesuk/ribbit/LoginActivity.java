5579f3127c74334751a226365be742a90b358149
harryjamesuk <harryjamesuk+github@gmail.com>
2014-09-26 17:31:50 +0100

    Hidden Action Bars
    Action Bars are now hidden in LoginActivity and SignUpActivity.

--- src/com/harryjamesuk/ribbit/LoginActivity.java
+++ src/com/harryjamesuk/ribbit/LoginActivity.java
@@ -1,10 +1,10 @@
 package com.harryjamesuk.ribbit;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -28,6 +28,9 @@ public class LoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 		setContentView(R.layout.activity_login);
+		
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
 		
 		mSignUpTextView = (TextView) findViewById(R.id.signUpText);
 		mSignUpTextView.setOnClickListener(new View.OnClickListener() {
