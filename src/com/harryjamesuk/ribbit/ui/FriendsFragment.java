8b95e770c12629575672dea0474f6e3540571966
harryjamesuk <harryjamesuk+github@gmail.com>
2014-10-31 17:03:36 +0000

    Converted ListView to GridView
    Just renamed some things to suit the new change. Now extending Fragment
    in FriendsFragment.
    Updated a few context marks here and there and added new GridView
    information.

--- src/com/harryjamesuk/ribbit/ui/FriendsFragment.java
+++ src/com/harryjamesuk/ribbit/ui/FriendsFragment.java
@@ -4,37 +4,43 @@ import java.util.List;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
 
+import com.harryjamesuk.ribbit.R;
+import com.harryjamesuk.ribbit.utils.ParseConstants;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
-import com.harryjamesuk.ribbit.R;
-import com.harryjamesuk.ribbit.R.layout;
-import com.harryjamesuk.ribbit.R.string;
-import com.harryjamesuk.ribbit.utils.ParseConstants;
 
-public class FriendsFragment extends ListFragment {
+public class FriendsFragment extends Fragment {
 	
 	public static final String TAG = FriendsFragment.class.getSimpleName();
 
 	protected ParseRelation<ParseUser> mFriendsRelation;
 	protected ParseUser mCurrentUser;	
 	protected List<ParseUser> mFriends;
+	protected GridView mGridView;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_friends,
 				container, false);
-
+		
+		mGridView = (GridView) rootView.findViewById(R.id.friendsGrid);
+		
+		TextView emptyTextView = (TextView) rootView.findViewById(android.R.id.empty);
+		mGridView.setEmptyView(emptyTextView);
+		
 		return rootView;
 	}
 
@@ -64,14 +70,14 @@ public class FriendsFragment extends ListFragment {
 						i++;
 					}
 					ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-							getListView().getContext(), 
+							getActivity(), 
 							android.R.layout.simple_list_item_1,
 							usernames);
-					setListAdapter(adapter);
+					mGridView.setAdapter(adapter);
 				}
 				else {
 					Log.e(TAG, e.getMessage());
-					AlertDialog.Builder builder = new AlertDialog.Builder(getListView().getContext());
+					AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 					builder.setMessage(e.getMessage())
 						.setTitle(R.string.error_title)
 						.setPositiveButton(android.R.string.ok, null);
