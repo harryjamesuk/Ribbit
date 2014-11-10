1334014c458a791fcc8441e03eb0d4d2b247474f
harryjamesuk <harryjamesuk+github@gmail.com>
2014-11-10 17:15:24 +0000

    Added checkmarks to EditFriendsActivity
    Converted ImageView to style.
    Set overlay ImageView as invisible by default.
    Added content desc to strings.xml
    
    In UserAdapter.java, declaring the GridView and setting the visibility
    of the overlay iamge to true or false dependant on whether the GridView
    item is checked or not.

--- src/com/harryjamesuk/ribbit/adapters/UserAdapter.java
+++ src/com/harryjamesuk/ribbit/adapters/UserAdapter.java
@@ -3,11 +3,11 @@ package com.harryjamesuk.ribbit.adapters;
 import java.util.List;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -36,6 +36,7 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
 			holder = new ViewHolder();
 			holder.userImageView = (ImageView)convertView.findViewById(R.id.userImageView);
 			holder.nameLabel = (TextView)convertView.findViewById(R.id.nameLabel);
+			holder.checkImageView = (ImageView)convertView.findViewById(R.id.checkImageView);
 			convertView.setTag(holder);
 		}
 		else {
@@ -56,19 +57,22 @@ public class UserAdapter extends ArrayAdapter<ParseUser> {
 			.into(holder.userImageView);
 		}
 		
-//		if (user.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)) {
-//			holder.iconImageView.setImageResource(R.drawable.ic_picture);
-//		}
-//		else {
-//			holder.iconImageView.setImageResource(R.drawable.ic_video);
-//		}
 		holder.nameLabel.setText(user.getUsername());
+		
+		GridView gridView = (GridView) parent;
+		if (gridView.isItemChecked(position)) {
+			holder.checkImageView.setVisibility(View.VISIBLE);
+		}
+		else {
+			holder.checkImageView.setVisibility(View.INVISIBLE);
+		}
 		
 		return convertView;
 	}
 	
 	private static class ViewHolder {
 		ImageView userImageView;
+		ImageView checkImageView;
 		TextView nameLabel;
 	}
 	
